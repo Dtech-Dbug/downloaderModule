ab3ce40631debb8c06bb47b52aa6e4ffda69b7b0
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-08 23:26:22 +0530

    created express routes

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const express = require("express");
-const app = express();
-const fs = require("fs");
-const ytdl = require("ytdl-core");
+// const fs = require("fs");
+// const ytdl = require("ytdl-core");
 const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/download", async (req, res) => {
-  videoURL = req.query.videoURL;
-
-  // ! passing encoded data of vdo
-  // ? fs writing file
-  ytdl(videoURL).pipe(fs.createWriteStream("video.mp4"));
-});
-
-app.get("/videoInfo", async (req, res) => {
-  console.log(res);
-  console.log(req.query.videoURL);
-  return;
-  videoURL = req.query.videoURL;
-  const videoInfo = await ytdl.getInfo(videoURL);
-  res.status(200).send(videoInfo);
-});
+//routes
+app.use("/api", routes);
 
 app.listen(PORT, () => {
-  console.log(`connected on port : ${PORT}`);
+  console.log(`RUNNING ON ${PORT}`);
 });
---
+++ routes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const router = express.Router();
+
+const PORT = process.env.PORT || 8000;
+
+router.get("/test", (req, res) => {
+  console.log("he");
+});
+
+router.get("/download", async (req, res) => {
+  videoURL = req.query.videoURL;
+
+  // ! passing encoded data of vdo
+  // ? fs writing file
+  ytdl(videoURL).pipe(fs.createWriteStream("video.mp4"));
+});
+
+router.get("/videoInfo", async (req, res) => {
+  console.log(req.query.videoURL);
+  return;
+  videoURL = req.query.videoURL;
+  const videoInfo = await ytdl.getInfo(videoURL);
+  res.status(200).send(videoInfo);
+});
+
+module.exports = router;
