3744947bdcaac10a310d0f57b068be81e6e2383b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-08 22:40:14 +0530

    updated server and installed vercel

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const ytdl = require("ytdl-core");
 const cors = require("cors");
 
 app.use(cors());
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 
-// ! passing encoded data of vdo
-// ? fs writing file
-ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(
-  fs.createWriteStream("video.mp4")
-);
+app.get("/download", async (req, res) => {
+  videoURL = req.query.videoURL;
+
+  // ! passing encoded data of vdo
+  // ? fs writing file
+  ytdl(videoURL).pipe(fs.createWriteStream("video.mp4"));
+});
 
 app.get("/videoInfo", async (req, res) => {
   console.log(res);
   console.log(req.query.videoURL);
+  return;
   videoURL = req.query.videoURL;
   const videoInfo = await ytdl.getInfo(videoURL);
   res.status(200).send(videoInfo);
