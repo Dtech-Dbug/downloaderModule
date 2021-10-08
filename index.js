7c8f08716d66d3c1d11931bcd53386980457501f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-09 00:31:02 +0530

    installed dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 // const ytdl = require("ytdl-core");
 const cors = require("cors");
 const routes = require("./routes");
+require("dotenv").config();
 
 const app = express();
 
